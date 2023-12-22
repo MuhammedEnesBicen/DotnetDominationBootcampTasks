3415e4044ca75a86d964dfbe04a8cbc670bce989
MuhammedEnesBicen <muhammedenesbicen49@gmail.com>
2023-12-22 21:07:00 +0300

    Add project files.

---
+++ Task1/Controllers/CountriesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Task1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountriesController : ControllerBase
+    {
+
+        [HttpGet()]
+        public IActionResult Get()
+        {
+            return Ok(new string[] { "Russia", "USA", "China", "Germany", "France", "Italy", "Spain", "Japan", "South Korea", "Canada", "Mexico", "Brazil", "Argentina", "Australia", "India", "Indonesia", "Turkey", "South Africa", "Egypt", "Nigeria" });
+        }
+
+        [HttpGet("{count}")]
+        public IActionResult Get(int count)
+        {
+            if (count < 1)
+            {
+                return BadRequest();
+            }
+            string[] countries = new string[] { "Russia", "USA", "China", "Germany", "France", "Italy", "Spain", "Japan", "South Korea", "Canada", "Mexico", "Brazil", "Argentina", "Australia", "India", "Indonesia", "Turkey", "South Africa", "Egypt", "Nigeria" };
+            if (count > countries.Length)
+            {
+                return StatusCode(StatusCodes.Status206PartialContent, countries);
+            }
+            return Ok(countries.Take(count));
+        }   
+    }
+}
---
+++ readme.md
@@ -0,0 +1,4 @@
+﻿# Dotnet Domination Bootcamp Tasks 
+Bu repo TechCareer.net tarafından düzenlenen [Dotnet Domination Bootcamp](https://www.techcareer.net/bootcamp/net-core-domination-bootcamp?utm_campaign=Techcareer_TC_Performance_Max&utm_source=google&utm_medium=cpc&gad_source=1&gclid=CjwKCAiAhJWsBhAaEiwAmrNyq4WrhzNKbco2SkLhdFyHhJjJf6kHdK2uH67uH2uJpWA--Bk-BRSolhoC-KgQAvD_BwE)'inde verilen görevler için oluşturulmuştur.
+
+task 1: 22.12.2023
